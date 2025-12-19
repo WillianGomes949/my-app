c07750cb19d0aa95fac7b193aff28ff10a4e9a07
Willian Gomes <146601824+WillianGomes949@users.noreply.github.com>
2025-12-19 13:43:21 -0300

    11

--- src/data/db.ts
+++ src/data/db.ts
@@ -172,15 +172,15 @@ const featuredProjects: Project[] = [
     tags: ["WordPress", "Elementor", "PHP"],
     liveUrl: "https://imobiliaria.williangomesdev.com/",
   },
-  {
-    id: 9,
-    title: "Clínica Saúde Total (WordPress)",
-    description:
-      "Demonstração de site para a área de saúde construído em WordPress. Focado em uma solução profissional que pode ser facilmente gerenciada pelo cliente usando Elementor.",
-    imageUrl: "/images/clinica-medica.webp",
-    tags: ["WordPress", "Elementor", "PHP"],
-    liveUrl: "https://portfolio-wordpress.infinityfreeapp.com/clinica-medica/",
-  },
+  // {
+  //   id: 9,
+  //   title: "Clínica Saúde Total (WordPress)",
+  //   description:
+  //     "Demonstração de site para a área de saúde construído em WordPress. Focado em uma solução profissional que pode ser facilmente gerenciada pelo cliente usando Elementor.",
+  //   imageUrl: "/images/clinica-medica.webp",
+  //   tags: ["WordPress", "Elementor", "PHP"],
+  //   liveUrl: "https://portfolio-wordpress.infinityfreeapp.com/clinica-medica/",
+  // },
 ];
 
 const allProjects: Project[] = [
